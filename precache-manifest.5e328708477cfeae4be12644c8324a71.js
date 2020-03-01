7010dc2d95ccdfe1f394c579c2d245d60b53c486
Manda <mandakadam@gmail.com>
2020-03-01 14:27:01 +0530

    Updates

---
+++ precache-manifest.5e328708477cfeae4be12644c8324a71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f326ffbf667ebee5a8a1d03a14f5bd",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "0e16eec9a507a942d10e",
+    "url": "/react-quiz-app/static/css/main.a96fbc94.chunk.css"
+  },
+  {
+    "revision": "1c9a6522eff3f8fc2241",
+    "url": "/react-quiz-app/static/js/2.aa01a76e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-quiz-app/static/js/2.aa01a76e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e16eec9a507a942d10e",
+    "url": "/react-quiz-app/static/js/main.7f018b54.chunk.js"
+  },
+  {
+    "revision": "7e34d9d9e85f94cfdc85",
+    "url": "/react-quiz-app/static/js/runtime-main.2b2f3000.js"
+  }
+]);
